10bbe5dc9848e43a718261e8f57e30e220b6403f
mervegok <mrvdka@gmail.com>
2025-06-02 19:59:22 +0200

    HelloWorld Updated

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -2,6 +2,5 @@ package day01;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World");
     }
-}
